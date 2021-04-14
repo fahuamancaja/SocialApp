c064d92a7252e6720abf3cec5952ef6b21d5f026
fahuamancaja <47445881+fahuamancaja@users.noreply.github.com>
2021-04-14 09:49:49 -0400

    End of Section 6

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -47,6 +47,7 @@ namespace API.Controllers
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if (user == null) return Unauthorized("Invalid username");
+            if(string.IsNullOrWhiteSpace(loginDto.Password)) return Unauthorized("Password Required");
 
             using var hmac = new HMACSHA512(user.PasswordSalt);
 
